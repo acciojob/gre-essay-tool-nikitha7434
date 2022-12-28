c29555d9d98c2d5fb8efcf140e093308fb76789e
Nikitha <nikithamashetti434@gmail.com>
2022-12-28 14:17:25 +0530

    adding file

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+function wordCount(){
+    var text = document.getElementById("evaluatedText").value;
+   
+  
+let splits =text.trim().split(' ');
+
+let count=0;
+
+for(let i=0;i<splits.length;i++){
+    
+    if(splits[i] != ""){
+        count ++;
+     }
+    let inres =document.getElementById("wordCount");
+    inres.innerHTML =count;
+    
+   
+}
+}
